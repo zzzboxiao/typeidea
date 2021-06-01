4c6b4661ac82fb49315ccec947818c129a9b4211
zzzboxiao <zhangbo@nationalchip.com>
2021-06-01 15:50:47 +0800

    配置页面通用数据

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -25,6 +25,21 @@ class Category(models.Model):
   def __str__(self):
     return self.name
 
+  @classmethod
+  def get_navs(cls):
+    categories = cls.objects.filter(status=cls.STATUS_NORMAL)
+    nav_categories = []
+    normal_categories = []
+    for cate in categories:
+      if cate.is_nav:
+        nav_categories.append(cate)
+      else:
+        normal_categories.append(cate)
+    return {
+      'navs': nav_categories,
+      'categories': normal_categories
+    }
+
 
 
 class Tag(models.Model):
@@ -66,6 +81,8 @@ class Post(models.Model):
   tag = models.ManyToManyField(Tag, verbose_name='标签')
   owner = models.ForeignKey(User, verbose_name='作者',on_delete=models.CASCADE) # 删除关联数据,与之关联也删除
   created_time = models.DateField(auto_now_add=True, verbose_name='创建时间')
+  pv = models.PositiveIntegerField(default=1)
+  uv = models.PositiveIntegerField(default=1)
   
   class Meta:
     ordering = ['-id']
@@ -73,3 +90,58 @@ class Post(models.Model):
 
   def __str__(self):
     return self.title
+
+    """
+
+      关联管理器(对象调用):
+      多对多: 双向均有关联管理器
+      一对多: 只有多的那个类的对象有关联管理器，即反向才有
+
+      正向查询: 属性名,             从主键表,去查询外键表(ManyToManyField或ForeignKey定义的Model类)或者表本身的数据
+      反向查询: 小写类名加 _set      从外键表去查询主键表
+
+      一对一的反向, 用 对象.小写类名 即可, 不用加 _set
+    """
+
+  # @staticmethod 该方法声明为静态方法,从而可以实现无需实例化即可调用
+  # 使用 @staticmethod 后, 不需要在添加实例化  def get_by_tag(self, tag_id):
+  @staticmethod
+  def get_by_tag(tag_id):
+    try:
+      tag = Tag.objects.get(id=tag_id)
+    except Tag.DoesNotExist:
+      tag = None
+      post_list = []
+    else:
+      """
+      反向查询, Tag 表 和 Post 表示多对多的关系，查询所有 Tag 是 tag ，同时状态为正常的文章
+      Post 主键表    Tag  外键表
+      class Post:
+        ......
+        tag = models.ManyToManyField(Tag, verbose_name='标签')
+        ......
+
+        使用select_related()方法一次性的把“表关联的对象”都查询出来放入对象中，再次查询时就不需要再连接数据库，节省了后面查询数据库的次数和时间。
+      """
+      post_list = tag.post_set.filter(status=Post.STATUS_NORMAL).select_related('owner','category')
+    return post_list, tag
+
+  @staticmethod
+  def get_by_category(category_id):
+    try:
+      category = Category.objects.get(id=category_id)
+    except Category.DoesNotExist:
+      category = None
+      post_list = []
+    else:
+      # select_related()的作用: post_list[0].owner.username
+      post_list = category.post_set.filter(status=Post.STATUS_NORMAL).select_related('owner','category')
+    return post_list, category
+
+  @classmethod
+  def latest_posts(cls):
+    queryset = cls.objects.filter(status=cls.STATUS_NORMAL)
+
+  @classmethod
+  def hot_posts(cls):
+    return cls.objects.filter(status=cls.STATUS_NORMAL).order_by('-pv')
--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Post, Tag
+from .models import Post, Tag, Category
+from config.models import SideBar
 
 
 def post_list(request, category_id=None, tag_id=None):
+  tag = None
+  category = None
   if tag_id:
-    try:
-      tag = Tag.objects.get(id=tag_id)
-    except Tag.DoesNotExist:
-      post_list = []
-    else:
-      post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+    post_list, tag = Post.get_by_tag(tag_id)
+  elif category_id:
+    post_list, category = Post.get_by_category(category_id)
   else:
-    post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
-    if category_id:
-      post_list = post_list.filter(category_id=category_id)
-  return render(request, 'blog/list.html', context={'post_list': post_list})
+    post_list = Post.latest_posts()
+
+  context = {
+    'category': category,
+    'tag': tag,
+    'post_list': post_list,
+    'sidebars': SideBar.get_all()
+  }
+  context.update(Category.get_navs())
+  return render(request, 'blog/list.html', context=context)
 
 
 def post_detail(request, post_id):
@@ -24,4 +30,9 @@ def post_detail(request, post_id):
     post = Post.objects.get(id=post_id)
   except Post.DoesNotExist:
     post = None
-  return render(request, 'blog/detail.html', context={'post': post})
+  context = {
+    'post': post,
+    'sidebars': SideBar.get_all()
+  }
+  context.update(Category.get_navs())
+  return render(request, 'blog/detail.html', context=context)
